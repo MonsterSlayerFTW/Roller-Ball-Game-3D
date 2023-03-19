cf85ef9253488538f26c52724100c047d9196cbd
MonsterFTW <70183820+MonsterSlayerFTW@users.noreply.github.com>
2023-03-19 15:12:37 -0700

    3/29/2023
    AI

--- Assets/Scripts/ChasingAI.cs
+++ Assets/Scripts/ChasingAI.cs
@@ -1,37 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class ChasingAI : MonoBehaviour
 {
-    public GameObject player;
-    public NavMeshAgent agent;
+    [SerializeField] private SphereCollider triggerSphere;
+    [SerializeField] private bool MoveRigidBody = true;
+    [SerializeField] private float RgbRotationSpeed = 5f;
+    [SerializeField] private float TriggerRadius = 5f;
+    [SerializeField] private float ChaseSpeed = 5f;
+    [SerializeField] private float ChaseDelay = 1f;
 
-    public float distance;
-    public bool isAngered;
+    private Transform localTrans;
+    private bool detected = false;
+    private Transform targetTrans;
+    private Vector3 targetPos;
+    private Rigidbody localRgb;
 
-    void Update()
+    void Start()
     {
-        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        localRgb= GetComponent<Rigidbody>();
+        localTrans = GetComponent<Transform>();
 
-        if(distance <= 5)
-        {
-            isAngered = true;
-        }
-        if(distance > 5)
-        {
-            isAngered = false;
-        }
+        if (!triggerSphere) triggerSphere = GetComponent<SphereCollider>();
+        if (triggerSphere)
+            triggerSphere.radius = TriggerRadius;
 
-        if(isAngered)
+        targetTrans = null;
+    }
+
+    private void Update()
+    {
+        if (detected && targetTrans != null)
         {
-            agent.isStopped = false;
-            agent.SetDestination(player.transform.position);
-        }
-        if(!isAngered)
-        {
-            agent.isStopped = true;
+            //ChasePlayer
+            Chase(targetTrans);
         }
     }
+
+    void Chase(Transform _target)
+    {
+        var Speed = ChaseSpeed;
+
+        targetPos = _target.position;
+        targetPos.y = localTrans.position.y;
+
+        //Move Rigibody
+        if (MoveRigidBody)
+        {
+            RotateRgb(_target);
+            localRgb.MovePosition(localRgb.position + localTrans.forward * Speed * Time.deltaTime);
+        }
+    }
+
+    private void RotateRgb(Transform _target)
+    {
+        Vector3 localTarget = localTrans.InverseTransformPoint(_target.position);
+
+        float angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+
+        Vector3 eulerAngleVelocity = new Vector3(0, angle, 0);
+        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime * RgbRotationSpeed);
+        localRgb.MoveRotation(localRgb.rotation * deltaRotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (detected) return;
+        //Follow only if the detected one is the PLayer
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Detected: " + other.name);
+            detected = true;
+            StartCoroutine(ActiveChasing(other.transform, ChaseDelay));
+        }
+    }
+
+    IEnumerator ActiveChasing(Transform other, float _waitSec = 1f)
+    {
+        yield return new WaitForSeconds(_waitSec);
+        targetTrans = other;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        //Uses the same var you use to draw your overlap sphere to whatever
+        Gizmos.DrawWireSphere(this.transform.position, TriggerRadius);
+    }
+
+    public void StopChasing()
+    {
+        detected = false;
+    }
+
 }
